db4fd8b45e687020ba41775c59ac984e247839fc
Jerome <je.bravais@gmail.com>
2021-06-13 20:09:35 +0200

    Use postgre instead of redis

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 // Includes
 const express = require('express');
-const redis = require("redis");
-const fs = require("fs");
+const { Client } = require('pg');
 
 // Constants
 const PORT = process.env.PORT || 5000;
 
-// Redis client
-const client = redis.createClient(process.env.REDIS_URL);
-
-client.on('connect', () => {
-    console.log('##########################################################');
-    console.log('#####            REDIS STORE CONNECTED               #####');
-    console.log('##########################################################\n');
-});
-
-client.on("error", function (err) {
-    console.log("Error " + err);
+// Postgre client
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Express App
@@ -31,32 +25,6 @@ app.get('/', function (req, res) {
 app.get('/ping', function (req, res) {
     console.log('ping recu');
     res.send('Pong');
-});
-
-app.get('/insert', async function (req, res) {
-    client.set("foo", "bar", function () { });
-    res.send("Ok");
-});
-
-app.get('/retrieve', async function (req, res) {
-    const key = "foo";
-
-    client.get(key, async (err, value) => {
-        if (value) {
-            return res.status(200).send({
-                error: false,
-                message: `Value for ${key} from the cache`,
-                data: JSON.parse(value)
-            });
-        }
-        else {
-            return res.status(200).send({
-                error: true,
-                message: `Key not found`,
-                data: JSON.parse(value)
-            });
-        }
-    })
 });
 
 app.listen(PORT, function () {
