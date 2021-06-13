50b34b3fc2b15432b0cbb1990af7c146954944c5
Jerome <je.bravais@gmail.com>
2021-06-13 18:18:06 +0200

    Add redis

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
+// Includes
+const express = require('express');
+const redis = require("redis");
+const { promisify } = require("util");
 
-const PORT = process.env.PORT || 5000
-const app = express()
+// Constants
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+// Express App
+const app = express();
+app.use(express.json());
 
+// Redis client
+const client = redis.createClient(process.env.REDIS_URL, {
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+// Express endpoints
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
@@ -14,6 +30,23 @@ app.get('/ping', function (req, res) {
     res.send('Pong');
 });
 
+app.get('/insert', async function (req, res) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('ping recu de ' + ip);
+
+    const value = await getAsync(ip, "toto");
+    console.log(value);
+
+    res.end();
+});
+
+app.get('/retrieve', async function (req, res) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const value = await getAsync(ip);
+    console.log(value);
+    res.send(value);
+});
+
 app.listen(PORT, function () {
-    console.log(`Listening on ${ PORT }`);
+    console.log(`Listening on ${PORT}`);
 });
