1f36e185a2ddaa6aa6c72b6ed2bc52a3673f3df0
Jerome <je.bravais@gmail.com>
2021-06-13 19:12:47 +0200

    Test redis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Includes
 const express = require('express');
 const redis = require("redis");
+const fs = require("fs");
 
 // Constants
 const PORT = process.env.PORT || 5000;
@@ -10,15 +11,15 @@ const app = express();
 app.use(express.json());
 
 // Redis client
-if (process.env.REDISTOGO_URL) {
-    var rtg = require("url").parse(process.env.REDISTOGO_URL);
-    var client = redis.createClient(rtg.port, rtg.hostname);
+const client = redis.createClient(process.env.REDIS_URL, {
+    tls: {
+        rejectUnauthorized: false
+    }
+});
 
-    redis.auth(rtg.auth.split(":")[1]);
-}
-else {
-    var client = redis.createClient();
-}
+client.on("error", (error) => {
+    console.error(error);
+});
 
 // Express endpoints
 app.get('/', function (req, res) {
@@ -32,14 +33,28 @@ app.get('/ping', function (req, res) {
 
 app.get('/insert', async function (req, res) {
     client.set("foo", "bar", function () { });
-    res.send(JSON.stringify({ result: "ok" }, null, 4));
+    res.send("Ok");
 });
 
 app.get('/retrieve', async function (req, res) {
+    const key = "foo";
 
-    client.get("foo", function (err, value) {
-        res.send(JSON.stringify({ 'key': "foo", 'value': value }, null, 4));
-    });
+    client.get(key, async (err, value) => {
+        if (value) {
+            return res.status(200).send({
+                error: false,
+                message: `Value for ${key} from the cache`,
+                data: JSON.parse(value)
+            });
+        }
+        else{
+            return res.status(200).send({
+                error: true,
+                message: `Key not found`,
+                data: JSON.parse(value)
+            });
+        }
+    })
 });
 
 app.listen(PORT, function () {
