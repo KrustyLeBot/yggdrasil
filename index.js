37fac48675d3650f21fd5e109f0e71a962c7999d
Jerome <37580288+KrustyLeBot@users.noreply.github.com>
2021-09-14 15:36:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,41 @@
 //////////////////////////////////////////////////////////////
 // Includes & constants
 const express = require('express');
-const pg = require('pg');
+const bodyParser = require('body-parser')
+
 const PORT = process.env.PORT || 5000;
 //////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////
-// Postgresql => Create a pool of 20 connections max
-// Replace xxxxx with URI for local tests
-const conString = process.env.DATABASE_URL || 'xxxxx';
-
-const pool = new pg.Pool({
-    connectionString: conString,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
-//////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////
-// Postgresql method
-const databaseExecute = async function (req) {
-    const client = await pool.connect()
-    const result = await client.query(req);
-    await client.end();
-    return result;
-};
-//////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////
-// Login restriction
-const checkLogin = function (login) {
-    return conString.includes(login);
-}
-//////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
 // Express app
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }))
 //////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
 // Express endpoints
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.get('/*', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
 
-app.get('/ping', function (req, res) {
-    res.send('Pong');
-});
+app.post('/*', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
 
-app.get('/:login/test', async function (req, res) {
-    // Dumb check to restrict users
-    if (!checkLogin(req.params.login)) {
-        return res.status(500).send('Unauthorized!');
-    }
+app.put('/*', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
 
-    const sql = "SELECT * FROM test";
-    const result = await databaseExecute(sql);
-    res.json(result.rows);
-});
+app.patch('/*', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
 //////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
