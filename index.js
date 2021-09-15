2bf37021d9941ddb137ebf3db24b5c6e49eac4c5
Jerome <37580288+KrustyLeBot@users.noreply.github.com>
2021-09-15 16:42:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,21 +19,25 @@ app.use(bodyParser.json({ limit: '50mb' }))
 // Express endpoints
 app.get('/*', (req, res) => {
   console.log(req.body)
+  console.log(req.headers);
   res.send(req.body)
 })
 
 app.post('/*', (req, res) => {
   console.log(req.body)
+  console.log(req.headers);
   res.send(req.body)
 })
 
 app.put('/*', (req, res) => {
   console.log(req.body)
+  console.log(req.headers);
   res.send(req.body)
 })
 
 app.patch('/*', (req, res) => {
   console.log(req.body)
+  console.log(req.headers);
   res.send(req.body)
 })
 //////////////////////////////////////////////////////////////
